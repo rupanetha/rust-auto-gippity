539ed46e97e97d1c51d1285f916f09e1d0e33e8d
Rupa Netha <iamsairupa06@gmail.com>
2025-06-24 12:20:46 +0530

    add functions in main

--- web-server-template/src/main.rs
+++ web-server-template/src/main.rs
@@ -1,67 +1,51 @@
-use actix_web::{web, HttpResponse, HttpServer, Responder};
+use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+use serde::{Deserialize, Serialize};
+use reqwest::Client as HttpClient;
+use async_trait::async_trait;
+
 use std::sync::Mutex;
 use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-struct FitnessProgress {
+struct ForexPair {
     id: u64,
-    user_id: u64,
-    workout: String,
-    duration: u64, // in minutes
-    timestamp: i64, // Unix timestamp
-    timezone: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct User {
-    id: u64,
-    username: String,
-    password: String,
-    timezone: String,
+    pair: String,
+    price: f64,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Database {
-    fitness_progresses: HashMap<u64, FitnessProgress>,
-    users: HashMap<u64, User>,
+    forex_pairs: HashMap<u64, ForexPair>,
 }
 
 impl Database {
     fn new() -> Self {
         Self {
-            fitness_progresses: HashMap::new(),
-            users: HashMap::new(),
+            forex_pairs: HashMap::new(),
         }
     }
 
-    fn insert_fitness_progress(&mut self, fitness_progress: FitnessProgress) {
-        self.fitness_progresses.insert(fitness_progress.id, fitness_progress);
+    fn insert(&mut self, forex_pair: ForexPair) {
+        self.forex_pairs.insert(forex_pair.id, forex_pair);
     }
 
-    fn get_fitness_progress(&self, id: &u64) -> Option<&FitnessProgress> {
-        self.fitness_progresses.get(id)
+    fn get(&self, id: &u64) -> Option<&ForexPair> {
+        self.forex_pairs.get(id)
     }
 
-    fn get_all_fitness_progresses(&self) -> Vec<&FitnessProgress> {
-        self.fitness_progresses.values().collect()
+    fn get_all(&self) -> Vec<&ForexPair> {
+        self.forex_pairs.values().collect()
     }
 
-    fn delete_fitness_progress(&mut self, id: &u64) {
-        self.fitness_progresses.remove(id);
+    fn delete(&mut self, id: &u64) {
+        self.forex_pairs.remove(id);
     }
 
-    fn update_fitness_progress(&mut self, fitness_progress: FitnessProgress) {
-        self.fitness_progresses.insert(fitness_progress.id, fitness_progress);
-    }
-
-    fn insert_user(&mut self, user: User) {
-        self.users.insert(user.id, user);
-    }
-
-    fn get_user_by_name(&self, username: &str) -> Option<&User> {
-        self.users.values().find(|u| u.username == username)
+    fn update(&mut self, forex_pair: ForexPair) {
+        self.forex_pairs.insert(forex_pair.id, forex_pair);
     }
 
     fn save_to_file(&self) -> std::io::Result<()> {
@@ -79,82 +63,74 @@ impl Database {
 }
 
 struct AppState {
-    db: Mutex<Database>,
+    db: Mutex<Database>
 }
 
-async fn create_fitness_progress(app_state: web::Data<AppState>, fitness_progress: web::Json<FitnessProgress>) -> impl Responder {
+async fn create_forex_pair(app_state: web::Data<AppState>, forex_pair: web::Json<ForexPair>) -> impl Responder {
     let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
-    db.insert_fitness_progress(fitness_progress.into_inner());
+    db.insert(forex_pair.into_inner());
     let _ = db.save_to_file();
     HttpResponse::Ok().finish()
 }
 
-async fn read_fitness_progress(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
+async fn read_forex_pair(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
     let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
-    match db.get_fitness_progress(&id.into_inner()) {
-        Some(fitness_progress) => HttpResponse::Ok().json(fitness_progress),
-        None => HttpResponse::NotFound().finish(),
+    match db.get(&id.into_inner()) {
+        Some(forex_pair) => HttpResponse::Ok().json(forex_pair),
+        None => HttpResponse::NotFound().finish()
     }
 }
 
-async fn read_all_fitness_progresses(app_state: web::Data<AppState>) -> impl Responder {
+async fn read_all_forex_pairs(app_state: web::Data<AppState>) -> impl Responder {
     let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
-    let fitness_progresses = db.get_all_fitness_progresses();
-    HttpResponse::Ok().json(fitness_progresses)
+    let forex_pairs = db.get_all();
+    HttpResponse::Ok().json(forex_pairs) 
 }
 
-async fn update_fitness_progress(app_state: web::Data<AppState>, fitness_progress: web::Json<FitnessProgress>) -> impl Responder {
+async fn update_forex_pair(app_state: web::Data<AppState>, forex_pair: web::Json<ForexPair>) -> impl Responder {
     let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
-    db.update_fitness_progress(fitness_progress.into_inner());
+    db.update(forex_pair.into_inner());
     let _ = db.save_to_file();
     HttpResponse::Ok().finish()
 }
 
-async fn delete_fitness_progress(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
+async fn delete_forex_pair(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
     let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
-    db.delete_fitness_progress(&id.into_inner());
+    db.delete(&id.into_inner());
     let _ = db.save_to_file();
     HttpResponse::Ok().finish()
 }
 
-async fn register(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
-    let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
-    db.insert_user(user.into_inner());
-    let _ = db.save_to_file();
-    HttpResponse::Ok().finish()
-}
-
-async fn login(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
-    let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
-    match db.get_user_by_name(&user.username) {
-        Some(stored_user) if stored_user.password == user.password => {
-            HttpResponse::Ok().body("Logged in")
-        },
-        _ => HttpResponse::BadRequest().body("Invalid username or password"),
-    }
-}
-
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let db = match Database::load_from_file() {
         Ok(db) => db,
-        Err(_) => Database::new(),
+        Err(_) => Database::new()
     };
 
     let data: web::Data<AppState> = web::Data::new(AppState {
-        db: Mutex::new(db),
+        db: Mutex::new(db)
     });
 
     HttpServer::new(move || {
         App::new()
-            .app_data(data.clone())
-            .route("/fitness_progress", web::post().to(create_fitness_progress))
-            .route("/fitness_progress", web::get().to(read_all_fitness_progresses))
-            .route("/fitness_progress/{id}", web::get().to(read_fitness_progress))
-            .route("/fitness_progress", web::put().to(update_fitness_progress))
-            .route("/fitness_progress/{id}", web::delete().to(delete_fitness_progress))
-            .route("/register", web::post().to(register))
-            .route("/login", web::post().to(login))
+            .wrap(
+                Cors::permissive()
+                    .allowed_origin_fn(|origin, _req_head| {
+                        origin.as_bytes().starts_with(b"http://localhost") || origin == "null"
+            })
+            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"]) 
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+             .allowed_header(header::CONTENT_TYPE)
+             .supports_credentials()
+             .max_age(3600)
+        )
+        .app_data(data.clone())
+        .route("/forex_pair", web::post().to(create_forex_pair))
+        .route("/forex_pair", web::get().to(read_all_forex_pairs))
+        .route("/forex_pair/{id}", web::get().to(read_forex_pair))
+        .route("/forex_pair", web::put().to(update_forex_pair))
+        .route("/forex_pair/{id}", web::delete().to(delete_forex_pair))
     })
     .bind("127.0.0.1:8080")?
     .run()
